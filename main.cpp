cba80f1fab4113bcb87637a8612ecc3079beb876
snailbaha <bahromer2005@gmail.com>
2023-03-18 20:24:32 +0300

    1...

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    string a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    char first = a[0];
+    char second = b[0];
+    char third = c[0];
+    char fourth = d[0];
+    char fifth = e[0];
+    cout << first << second << third << fourth << fifth;
     return 0;
 }
